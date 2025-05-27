19fa5a89cec9710a0127ae7514fac906fbbb0057
Keith Yu <kyu3@bu.edu>
2025-05-27 17:39:17 -0400

    syntax

--- static/manager.js
+++ static/manager.js
@@ -28,7 +28,7 @@ function fetchWhitelist(){
                 <td>${entry.access}</td>
                 <td>${entry.host}</td>
                 <td>${entry.last_used}</td>
-                <td><img src="/static/images/test.jpg" alt="User Image" width="60"></td>
+                <td><img src="/static/images/shahid.png" alt="User Image" width="60"></td>
                 <td><button onclick = "deleteEntry('${entry.uid}')">Delete</button></td>
                 `;
                 whitelist.appendChild(row);
